966cf8d9ea0953765a844f10e541afee6157237d
COCO <hong.git@yahoo.com>
2025-01-19 21:47:40 +0900

    css

--- nico-app/src/App.js
+++ nico-app/src/App.js
@@ -1,8 +1,11 @@
+import Button from "./Button";
+
 
 function App() {
   return (
     <div>
       <h1>welcome back!</h1>
+      <Button text={"ddd"} />
     </div>
   );
 }
---
+++ nico-app/src/Button.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+function Button({ text }) {
+    return <button>{text}</button>;
+}
+
+Button.prototype = {
+    text: PropTypes.string.isRequired,
+};  
+
+export default Button;
